765b9e46d08e1b6bcdf6ff5bc059d003e20847a1
LuisJavier23 <luisjavier23n@hotmail.com>
2019-04-18 20:35:00 -0500

    Generic Commit

--- resources/views/searchbar/searchbar.blade.php
+++ resources/views/searchbar/searchbar.blade.php
@@ -194,12 +194,7 @@
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-          <li class="nav-item">
-                <a class="nav-link" href="#">Done</a>
-              </li>
+      
           
         </ul>
       </div>
