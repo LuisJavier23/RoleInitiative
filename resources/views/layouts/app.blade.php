812871f813563ad8aa733708d16aa831167c7d46
LuisJavier23 <luisjavier23n@hotmail.com>
2019-05-28 09:00:14 -0500

    Generic Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,8 +63,6 @@
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- Search -->
 
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
 </head>
 <body>
