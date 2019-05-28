4d894d056b88b5cbe9290e04544ca021261d8a7e
LuisJavier23 <luisjavier23n@hotmail.com>
2019-05-27 23:55:22 -0500

    Generic Commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
