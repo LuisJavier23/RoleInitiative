7373fa9fe2fe23c2f70fcb3f7f647112ee5a24a3
LuisJavier23 <luisjavier23n@hotmail.com>
2019-05-28 00:44:23 -0500

    Generic Commit

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,4 @@ Route::get('/landing', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('new-project', array('uses' => 'AutoLoginController@load'));
