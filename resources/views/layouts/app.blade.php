65f4a96ba4d0e304cd09e36c39f1659462b7004c
LuisJavier23 <luisjavier23n@hotmail.com>
2019-05-29 11:52:50 -0500

    Generic Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -171,7 +171,7 @@
             </style>
     
     
-     <div class="topnav">
+     <div class="topnav"> <!-- test-->
             <a href="{{ url('/characters') }}" style="font-family: BreatheFire;font-style: italic;font-size: 20px;">Characters</a>
             <a href="#Campains" style="font-family: BreatheFire;font-style: italic;font-size: 20px;">Campains</a>
             <a href="{{ url('/auth/logout') }}" style="font-family: BreatheFire;font-style: italic;font-size: 20px;">Logout</a>
